724ca74e7c9433494b77c541ef129b4f81a6ed37
Aleksandr Fisenko <a.fisenko@tg.dunice.net>
2025-01-29 14:07:37 +0300

    fix: remove snowfall

--- src/routes/layouts.tsx
+++ src/routes/layouts.tsx
@@ -1,23 +1,15 @@
 import { Outlet } from 'react-router-dom';
-import Snowfall from 'react-snowfall';
 import Header from '../components/Header';
 import MobileBottomNavigation from '../components/BottomNavigation';
-import classes from './layouts.module.css';
 
 export const AuthLayout = () => (
   <>
-    <div className={classes.snowfall}>
-      <Snowfall color="#964b00" />
-    </div>
     <Outlet />
   </>
 );
 
 export const MainLayout = () => (
   <>
-    <div className={classes.snowfall}>
-      <Snowfall color="#964b00" />
-    </div>
     <Header />
     <Outlet />
     <MobileBottomNavigation />
